c2e35102612a32f12826ff47bfd57fbe11a03927
büşra yüksel <busraa.yksel@outlook.de>
2021-02-07 17:24:36 +0100

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] numbers;
+        V[] names;
+        public MyDictionary()
+        {
+            numbers = new K[0];
+            names = new V[0];
+        }
+        public void Add(K number, V name)
+        {
+            K[] tempArray1= numbers;
+            numbers = new K[numbers.Length + 1];
+            
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                numbers[i] = tempArray1[i];
+            }
+
+            numbers[numbers.Length - 1] = number;
+
+            V[] tempArray2 = names;
+            names = new V[names.Length + 1];
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                names[i] = tempArray2[i];
+            }
+
+            names[names.Length - 1] = name;
+        }
+
+        public int Length1
+        {
+            get { return numbers.Length; }
+        }
+        public K[] Numbers
+        {
+            get { return numbers; }
+        }
+
+        public int Length2
+        {
+            get { return names.Length; }
+        }
+        public V[] Names
+        {
+            get { return names; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenciler = new MyDictionary<int, string>();
+            ogrenciler.Add(123, "BÜŞRA");
+            Console.WriteLine(ogrenciler.Length1);
+
+            ogrenciler.Add(345, "MELİSA");
+            Console.WriteLine(ogrenciler.Length2);
+
+            foreach (var number in ogrenciler.Numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            foreach (var name in ogrenciler.Names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
